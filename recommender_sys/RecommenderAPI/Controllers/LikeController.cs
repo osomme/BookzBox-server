82b94bfeb23c289a98be02f58bcf3262d1e7575d
htskole <38259894+HTSkole@users.noreply.github.com>
2020-03-11 01:46:23 +0100

    Added like relationship

---
+++ recommender_sys/RecommenderAPI/Controllers/LikeController.cs
@@ -0,0 +1,45 @@
+using System.Threading.Tasks;
+using BooxBox.DataAccess.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace BookzBox.Controllers
+{
+    [ApiController]
+    public class LikeController : ControllerBase
+    {
+        private readonly LikeRepository _likeRepo;
+
+        public LikeController()
+        {
+            _likeRepo = new LikeRepository();
+        }
+
+        /// <summary>Adds the passed box to the database.</summary>
+        /// <param name="key">The API key.</param>
+        /// <param name="userId">The id of the user who likes the box.</param>
+        /// <param name="boxId">The id of the box to like.</param> 
+        /// <returns>
+        ///     <see cref="BadRequestResult"/> if the passed data fails validation.
+        ///     <see cref="ForbidResult"/> if the API key is not valid.
+        ///     <see cref="OkResult"/> if successful.
+        /// </returns>
+        [HttpGet("api/like")]
+        public async Task<IActionResult> LikeAsync(string key, string userId, string boxId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            if (!ApiKey.IsValid(key))
+            {
+                return Forbid();
+            }
+
+            await _likeRepo.LikeAsync(userId, boxId);
+
+            return Ok();
+        }
+    }
+}
---
+++ recommender_sys/RecommenderAPI/DataAccess/Repositories/LikeRepository.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Threading.Tasks;
+using Models;
+using Neo4j.Driver;
+
+namespace BooxBox.DataAccess.Repositories
+{
+    public class LikeRepository : BaseRepository
+    {
+        public async Task LikeAsync(string userId, string boxId)
+        {
+            try
+            {
+                IResultCursor cursor = await _database.Session.RunAsync(
+                    $"MATCH (u:User {{userId: '{userId}'}}),(b:Box {{boxId: '{boxId}'}}) MERGE (u)-[:LIKES]-(b)"
+                );
+                await cursor.ConsumeAsync();
+            }
+            finally
+            {
+                await _database.CloseSessionAsync();
+            }
+        }
+    }
+}
