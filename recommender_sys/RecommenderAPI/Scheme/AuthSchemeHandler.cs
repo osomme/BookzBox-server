d15b6ed338ecbd8b21bff4a1f20056ac023d16e0
htskole <38259894+HTSkole@users.noreply.github.com>
2020-03-22 08:40:29 +0100

    Auth scheme for forbidden result

---
+++ recommender_sys/RecommenderAPI/Scheme/AuthSchemeHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace RecommenderAPI.Scheme
+{
+    public class AuthSchemeHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public AuthSchemeHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger,
+            UrlEncoder encoder, ISystemClock clock)
+            : base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync() =>
+            Task.FromResult(AuthenticateResult.NoResult());
+    }
+}
